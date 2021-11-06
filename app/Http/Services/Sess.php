bec5d0c2193a6013f85ef39e44a2e451544b8413
greylight2 <ya-vam-ne@mail.ru>
2021-11-06 19:46:58 +0300

    Создан обработчик сессии

---
+++ app/Http/Services/Sess.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Services;
+
+class Sess
+{
+  public static function set(string $key, $value): void
+  {
+    session($key, serialize($value));
+  }
+
+  public static function get(string $key)
+  {
+    return unserialize(session($key));
+  }
+}
