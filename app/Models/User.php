58b7d1879bd2291b8d1e490a70fcd036769b82af
greylight2 <ya-vam-ne@mail.ru>
2021-11-06 20:18:08 +0300

    создан файл User

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use App\Http\Services\Sess;
+use Illuminate\Http\Request;
 
-class User extends Authenticatable
+class User
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    private $numberList = [];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    public function __construct(Request $request)
+    {
+        if ($request->session()->has('numberList')) {
+            Sess::get('numberList');
+        }
+    }
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public function addNumber(int $number): void
+    {
+        $this->numberList[] = $number;
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+        Sess::set('numberList', $this->numberList);
+    }
+
+    public function getNumberList(): array
+    {
+        return $this->numberList;
+    }
 }
